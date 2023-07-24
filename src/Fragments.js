40f8152d6c3466cde82fecda0637f2972f72dbbb
unknown <smeena1@es.iitr.ac.in>
2023-07-24 13:23:46 +0530

    Practice repo

---
+++ src/Fragments.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Fragments() {
+  return (
+    <React.Fragment>
+        {/* <div className='box marginBottem'>Fragments</div> */}
+        <h1>React Fragment</h1>
+        <h1>React Fragment</h1>
+    </React.Fragment>
+  )
+}
+
+export default Fragments
+
+//JSX elements should be wraped in div tag enclosing tag
